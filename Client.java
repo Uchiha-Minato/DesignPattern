e96ae269f01488b7d0b1c1512114ecddc2b0a215
UchihaMinato <longhy513@163.com>
2022-05-02 02:39:42 +0800

    Client update

--- Client.java
+++ Client.java
@@ -2,12 +2,18 @@ package factoryMethodPattern;
 
 import java.util.Scanner;
 
+/**
+ * Client 模拟客户端
+ * */
 public class Client {
 
     public static ILogFactory logFactory;
     public static ILog log;
 
     public static void main(String[] args) {
+        System.out.println("------日志记录器-------");
+        System.out.println("1.文件记录\n2.数据库记录");
+        System.out.println("请输入选择");
         Scanner scanner = new Scanner(System.in);
         int choose = scanner.nextInt();
 
